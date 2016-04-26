0efd758a050f32b9f5e1a59a1d50483e9e7c4109
chmoreau <charles.moreau@insa-lyon.fr>
2016-04-26 13:13:45 +0200

    service getPlaces fetches data from dummy json file

--- app/services/app.service.ts
+++ app/services/app.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from 'angular2/core';
 import {Place} from '../models/place';
-import {Http} from 'angular2/http';
+import {Http, Response} from 'angular2/http';
 import {Observable}     from 'rxjs/Observable';
 
 @Injectable()
