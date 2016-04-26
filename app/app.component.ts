9ca121089a83aecf60330589817eb2731dc76501
Charles Moreau <Charles Moreau>
2016-04-26 09:32:20 +0200

    add search component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,9 @@
 import {Component} from 'angular2/core';
-
+import {SearchComponent} from './components/search.component';
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `<h1>My First Angular 2 App</h1>
+    	<search-component><search-component>`,
+    directives: [SearchComponent]
 })
 export class AppComponent { }
---
+++ app/components/search.component.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core';
+
+@Component({
+	selector: 'search-component',
+	templateUrl: 'app/templates/search.component.html',
+
+})
+export class SearchComponent {
+}
