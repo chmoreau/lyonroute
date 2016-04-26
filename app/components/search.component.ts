8e168fd24a451177bd60a1ba327fc6b32c9d758b
Charles Moreau <Charles Moreau>
2016-04-26 10:25:03 +0200

    added prime ng framework

--- app/components/search.component.ts
+++ app/components/search.component.ts
@@ -1,9 +1,20 @@
 import {Component} from 'angular2/core';
+import {Checkbox, Button, RadioButton} from 'primeng/primeng';
+
+
+
 
 @Component({
 	selector: 'search-component',
 	templateUrl: 'app/templates/search.component.html',
+	directives: [Checkbox, Button, RadioButton],
 
 })
 export class SearchComponent {
+	value: boolean = true;
+	selectedValue: string;
+
+	onclick() {
+		console.log("click");
+	}
 }
