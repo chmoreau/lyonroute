caebf533caaace51eeab66b0194b2f2dd39bc4e3
scriam <tertiuc@gmail.com>
2016-04-28 09:55:48 +0200

    offerRide

--- app/components/offerRide.component.ts
+++ app/components/offerRide.component.ts
@@ -9,12 +9,14 @@ import {MdButton} from '@angular2-material/button';
 import {MD_CARD_DIRECTIVES} from '@angular2-material/card';
 import {MD_INPUT_DIRECTIVES} from '@angular2-material/input';
 import {MD_LIST_DIRECTIVES} from '@angular2-material/list';
-
+import {MdRadioButton} from '@angular2-material/radio';
+import {MdRadioDispatcher} from '@angular2-material/radio/radio_dispatcher';
 
 @Component({
     selector: 'my- app1',
     templateUrl: `app/templates/offerRide.component.html`,
-	directives: [MdButton, MD_INPUT_DIRECTIVES, MD_CARD_DIRECTIVES, AutoComplete]
+	directives: [MdButton, MD_INPUT_DIRECTIVES, MD_CARD_DIRECTIVES, AutoComplete,MdRadioButton],
+	providers: [MdRadioDispatcher]
 })
 
 export class OfferRideComponent {}
