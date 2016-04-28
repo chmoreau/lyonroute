1dea10b84d1196759932894fc4e69c540591f3bf
scriam <tertiuc@gmail.com>
2016-04-28 10:33:22 +0200

    calendar

--- app/components/offerRide.component.ts
+++ app/components/offerRide.component.ts
@@ -1,4 +1,4 @@
-import {Component} from 'angular2/core';
+import {Component,OnInit} from 'angular2/core';
 import {AppService} from '../services/app.service'
 import {Place} from '../models/place'
 import { Router } from 'angular2/router';
@@ -11,12 +11,39 @@ import {MD_INPUT_DIRECTIVES} from '@angular2-material/input';
 import {MD_LIST_DIRECTIVES} from '@angular2-material/list';
 import {MdRadioButton} from '@angular2-material/radio';
 import {MdRadioDispatcher} from '@angular2-material/radio/radio_dispatcher';
+import {Calendar} from 'primeng/primeng';
+import {MdCheckbox} from '@angular2-material/checkbox';
 
 @Component({
     selector: 'my- app1',
     templateUrl: `app/templates/offerRide.component.html`,
-	directives: [MdButton, MD_INPUT_DIRECTIVES, MD_CARD_DIRECTIVES, AutoComplete,MdRadioButton],
+	directives: [MdButton, MD_INPUT_DIRECTIVES, MD_CARD_DIRECTIVES, AutoComplete, MdRadioButton, Calendar,MdCheckbox],
 	providers: [MdRadioDispatcher]
 })
 
-export class OfferRideComponent {}
+export class OfferRideComponent implements OnInit{
+  	es: any;
+	date11: string;
+
+    ngOnInit() {
+        this.es = {
+            closeText: "Cerrar",
+        	prevText: "<Ant",
+        	nextText: "Sig>",
+        	currentText: "Hoy",
+        	monthNames: [ "enero","febrero","marzo","abril","mayo","junio",
+        	"julio","agosto","septiembre","octubre","noviembre","diciembre" ],
+        	monthNamesShort: [ "ene","feb","mar","abr","may","jun",
+        	"jul","ago","sep","oct","nov","dic" ],
+        	dayNames: [ "domingo","lunes","martes","miércoles","jueves","viernes","sábado" ],
+        	dayNamesShort: [ "dom","lun","mar","mié","jue","vie","sáb" ],
+        	dayNamesMin: [ "D","L","M","X","J","V","S" ],
+        	weekHeader: "Sm",
+        	dateFormat: "dd/mm/yy",
+        	firstDay: 1,
+        	isRTL: false,
+        	showMonthAfterYear: false,
+        	yearSuffix: "" 
+        };
+    }
+}
