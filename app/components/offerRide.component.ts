1f0a12b9cad96dc2d13adc0597787e85dd58fa26
chmoreau <charles.moreau@insa-lyon.fr>
2016-04-28 16:13:01 +0200

    fix duplicate import

--- app/components/offerRide.component.ts
+++ app/components/offerRide.component.ts
@@ -2,9 +2,7 @@ import {Component,OnInit} from 'angular2/core';
 import {AppService} from '../services/app.service'
 import {Place} from '../models/place'
 import { Router } from 'angular2/router';
-
 import {AutoComplete} from 'primeng/primeng';
-import {MdCheckbox} from '@angular2-material/checkbox';
 import {MdButton} from '@angular2-material/button';
 import {MD_CARD_DIRECTIVES} from '@angular2-material/card';
 import {MD_INPUT_DIRECTIVES} from '@angular2-material/input';
